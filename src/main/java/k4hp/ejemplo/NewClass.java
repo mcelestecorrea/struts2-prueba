84e33dab4f0db73a3d9d3f342aec328eb8ea9b13
angelowolf <angelowolf21@gmail.com>
2016-04-07 21:47:46 -0300

    test

---
+++ src/main/java/k4hp/ejemplo/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package k4hp.ejemplo;
+
+/**
+ *
+ * @author angelo
+ */
+public class NewClass {
+    
+}
